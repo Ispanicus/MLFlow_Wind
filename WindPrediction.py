254dc93816aeec19979b1c65a4d91688622cf60b
Ispanicus <36916264+Ispanicus@users.noreply.github.com>
2021-04-21 10:08:16 +0200

    Update WindPrediction.py

--- WindPrediction.py
+++ WindPrediction.py
@@ -19,7 +19,7 @@ from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 mlflow.set_tracking_uri('http://training.itu.dk:5000/')
 mlflow.sklearn.autolog()
 
-with mlflow.start_run(run_name="alai"):
+with mlflow.start_run():
     
     dataset_time = datetime.datetime.now()
     mlflow.log_param("dataset_time", dataset_time)
